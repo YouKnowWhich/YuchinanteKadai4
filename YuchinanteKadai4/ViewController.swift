e8494962e2bd963c719511128d6be169e607a9d5
yuya-3110 <ysuu45143745@gmail.com>
2023-10-07 15:42:54 +0900

    first commit

--- YuchinanteKadai4/ViewController.swift
+++ YuchinanteKadai4/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var calculationLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private var count = 0
+
+    // ボタンがタップされたときに呼び出されるメソッド
+    @IBAction private func countUpBtn(_ sender: Any) {
+        // カウントを1増やし、ラベルに表示
+        count += 1
+        calculationLabel.text = String(count)
     }
 
-
+    // ボタンがタップされたときに呼び出されるメソッド
+    @IBAction private func countClearBtn(_ sender: Any) {
+        // カウントを0にリセットし、ラベルに表示
+        count = 0
+        calculationLabel.text = String(count)
+    }
 }
-
